e64aaad9d8d55d33f51a9249d8a25c3d0bb24e87
RiP3rQ <rafalpompa2000@gmail.com>
2023-01-02 20:33:58 +0100

    home screen + NativeWind setup (tailwindcss)

---
+++ screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="p-5">
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
