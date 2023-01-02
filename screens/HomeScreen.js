88ea9f58b47638153040f3aa111148df36dd82c3
RiP3rQ <rafalpompa2000@gmail.com>
2023-01-02 22:30:47 +0100

    google autocomplete library implementation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,21 +2,60 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView className="h-full bg-white">
       <View className="p-5">
         <Image
           style={{
-            width: 120,
-            height: 120,
+            width: 100,
+            height: 100,
             resizeMode: "contain",
           }}
           source={{
             uri: "https://icons-for-free.com/iconfiles/png/512/uber-1324440247504689178.png",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePlacesSearch"
+          placeholder="Where from?"
+          debounce={400}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          minLength={2}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          returnKeyType={"search"}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
