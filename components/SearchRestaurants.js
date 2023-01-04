7748ab5c10dcf2c97d8bebfc562e6b8bf402a06c
RiP3rQ <rafalpompa2000@gmail.com>
2023-01-04 20:17:31 +0100

    SearchRestaurants component (1/2)

--- components/SearchRestaurants.js
+++ components/SearchRestaurants.js
@@ -1,10 +1,68 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesing from "react-native-vector-icons/AntDesign";
+import { useRef } from "react";
 
 const SearchRestaurants = () => {
+  const autoComplete = useRef(null);
   return (
-    <View className="">
-      <Text>SearchRestaurants</Text>
+    <View className="mt-4 flex-row">
+      <GooglePlacesAutocomplete
+        ref={autoComplete}
+        nearbyPlacesAPI="GooglePlacesSearch"
+        placeholder="Search restaurants..."
+        debounce={400}
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language: "en",
+        }}
+        minLength={2}
+        enablePoweredByContainer={false}
+        returnKeyType={"search"}
+        renderLeftButton={() => (
+          <View className="ml-2">
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        textInputProps={{ clearButtonMode: "never" }}
+        renderRightButton={() => (
+          <View className="flex-row items-center">
+            <TouchableOpacity
+              onPress={() => {
+                autoComplete.current.clear();
+              }}
+            >
+              <Ionicons name="close-circle-outline" size={24} />
+            </TouchableOpacity>
+            <View className="flex-row mr-2 bg-white p-2 rounded-3xl items-center">
+              <AntDesing
+                name="clockcircle"
+                size={11}
+                style={{ marginRight: 6 }}
+              />
+              <Text>Search</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
