7221ee9644c5cacfc7e3edbaae21a22860d9b948
RiP3rQ <rafalpompa2000@gmail.com>
2023-01-05 21:14:19 +0100

    Making About.js Dynamic

--- components/RestaurantDetail/About.js
+++ components/RestaurantDetail/About.js
@@ -1,16 +1,27 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 
-const image =
-  "https://www.blueosa.com/wp-content/uploads/2020/01/the-best-top-10-indian-dishes.jpg";
-const title = "Essa dobra Restauracja Koks";
-const description = "Thai . Comfort Food . $$ . 🎟 . 4 ⭐(2914+) ";
+const yelpRestaurantInfo = {
+  name: "Essa dobra Restauracja Koks",
+  image:
+    "https://www.blueosa.com/wp-content/uploads/2020/01/the-best-top-10-indian-dishes.jpg",
+  price: "$$",
+  reviews: "1500",
+  rating: 4.9,
+  categories: [{ title: "Thai" }, { title: "Comfort Food" }],
+};
+
+const { name, image, price, reviews, rating, categories } = yelpRestaurantInfo;
+const formattedCategories = categories.map((cat) => cat.title).join(" · ");
+const description = `${formattedCategories} ${
+  price ? " · " + price : ""
+} · 🎟  · ${rating}⭐ · (${reviews}+)`;
 
 const About = () => {
   return (
     <View>
       <RestaurantImage image={image} />
-      <RestaurantTitle title={title} />
+      <RestaurantTitle name={name} />
       <RestaurantDescription description={description} />
     </View>
   );
@@ -22,8 +33,8 @@ const RestaurantImage = ({ image }) => (
   <Image source={{ uri: image }} className="w-100 h-48" />
 );
 
-const RestaurantTitle = ({ title }) => (
-  <Text className="font-bold text-3xl mt-2 mx-4">{title}</Text>
+const RestaurantTitle = ({ name }) => (
+  <Text className="font-bold text-3xl mt-2 mx-4">{name}</Text>
 );
 
 const RestaurantDescription = ({ description }) => (
